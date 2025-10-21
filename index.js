08e1ed7ba5ab7cff053a42aa292e471e61c10248
atinseau <arthurtinseau@live.fr>
2025-10-21 22:37:13 +0200

    syncro

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
+  console.log(req.body)
   res.send('Hello World!')
 })
 
